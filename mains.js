2b97246fa293da5745ffc20c6abd409e2633fef6
Agustin-garcia13 <agustingarciaaltamura@gmail.com>
2023-08-09 18:38:13 -0300

    correcciones segunda entrega

--- mains.js
+++ mains.js
@@ -1,60 +1,80 @@
+function startCurrencyConverter() {
+    while (true) {
+        convertCurrency();
+
+        const restart = confirm("¿Deseas realizar otra conversión?");
+        if (!restart) {
+            alert("¡Gracias por usar el conversor de divisas!");
+            break; // Sal del bucle si el usuario no desea reiniciar
+        }
+    }
+}
+
 function convertCurrency() {
-  const amountStr = prompt("Ingresa la cantidad:");
-  const amount = parseFloat(amountStr);
+    const amountStr = prompt("Ingresa la cantidad:");
+    const amount = parseFloat(amountStr);
 
-  if (isNaN(amount)) {
-      alert("Ingresa una cantidad válida.");
-      return;
-  }
+    if (isNaN(amount)) {
+        alert("Ingresa una cantidad válida.");
+        return;
+    }
 
-  const currencies = ["USD", "EUR", "JPY"]; 
+    const currencies = ["USD", "EUR", "JPY"]; // Agrega más divisas aquí según tus necesidades
 
-  let fromCurrency;
-  let toCurrency;
+    let fromCurrency;
+    let toCurrency;
 
-  do {
-      fromCurrency = promptMenu("Elige la divisa de origen:", currencies);
-  } while (!fromCurrency);
+    do {
+        fromCurrency = promptMenu("Elige la divisa de origen:", currencies);
+        if (!currencies.includes(fromCurrency)) {
+            alert("Divisa de origen no válida. Por favor, elige una opción válida.");
+        }
+    } while (!currencies.includes(fromCurrency));
 
-  do {
-      toCurrency = promptMenu("Elige la divisa de destino:", currencies);
-  } while (!toCurrency);
+    do {
+        toCurrency = promptMenu("Elige la divisa de destino:", currencies);
+        if (!currencies.includes(toCurrency)) {
+            alert("Divisa de destino no válida. Por favor, elige una opción válida.");
+        }
+    } while (!currencies.includes(toCurrency));
 
-  // Tasas de cambio fijas 
-  const exchangeRates = {
-      USD: { EUR: 0.85, JPY: 110.25 },
-      EUR: { USD: 1.18, JPY: 129.71 },
-      JPY: { USD: 0.0091, EUR: 0.0077 }
-  };
+    // Resto del código para realizar la conversión y mostrar el resultado
+    // ...
 
-  if (fromCurrency === toCurrency) {
-      alert("Las divisas de origen y destino son iguales. No es necesario convertir.");
-      return;
-  }
+    // Ejemplo de cálculo de la conversión basado en tasas de cambio (array de objetos)
+    const exchangeRates = [
+        { from: "USD", to: "EUR", rate: 0.85 },
+        { from: "USD", to: "JPY", rate: 110.25 },
+        { from: "EUR", to: "USD", rate: 1.18 },
+        { from: "EUR", to: "JPY", rate: 129.71 },
+        { from: "JPY", to: "USD", rate: 0.0091 },
+        { from: "JPY", to: "EUR", rate: 0.0077 }
+        // Agrega más tasas de cambio según tus necesidades
+    ];
 
-  if (!exchangeRates[fromCurrency] || !exchangeRates[fromCurrency][toCurrency]) {
-      alert("Las tasas de cambio no están disponibles para estas divisas.");
-      return;
-  }
+    const rateObj = exchangeRates.find(rate => rate.from === fromCurrency && rate.to === toCurrency);
 
-  const convertedAmount = amount * exchangeRates[fromCurrency][toCurrency];
-  alert(`${amount.toFixed(2)} ${fromCurrency} equivale a ${convertedAmount.toFixed(2)} ${toCurrency}.`);
+    if (!rateObj) {
+        alert("Las tasas de cambio no están disponibles para estas divisas.");
+        return;
+    }
+
+    const convertedAmount = amount * rateObj.rate;
+    alert(`${amount.toFixed(2)} ${fromCurrency} equivale a ${convertedAmount.toFixed(2)} ${toCurrency}.`);
 }
 
 function promptMenu(message, options) {
-  const userInput = prompt(`${message}\n${options.map((option, index) => `${index + 1}. ${option}`).join("\n")}`);
-  const selectedOption = parseInt(userInput);
-  switch (selectedOption) {
-      case 1:
-          return options[0];
-      case 2:
-          return options[1];
-      case 3:
-          return options[2];
-      default:
-          return null;
-  }
+    const userInput = prompt(`${message}\n${options.map((option, index) => `${index + 1}. ${option}`).join("\n")}`);
+    const selectedOption = parseInt(userInput);
+    
+    if (!isNaN(selectedOption)) {
+        return options[selectedOption - 1];
+    } else {
+        return null;
+    }
 }
 
-// Llama a la función para iniciar la conversión cuando se ejecute el programa
-convertCurrency();
+startCurrencyConverter();
+
+
+
