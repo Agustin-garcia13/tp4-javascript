0ca89a1e225498074b3719ae0d2c96887b810f91
Agustin-garcia13 <agustingarciaaltamura@gmail.com>
2023-09-02 16:23:23 -0300

    cambios para la entrega final

--- mains.js
+++ mains.js
@@ -1,31 +1,41 @@
+ //array de objetos
+ const exchangeRates = [
+    { from: "USD", to: "EUR", rate: 0.85 },
+    { from: "USD", to: "JPY", rate: 110.25 },
+    { from: "EUR", to: "USD", rate: 1.18 },
+    { from: "EUR", to: "JPY", rate: 129.71 },
+    { from: "JPY", to: "USD", rate: 0.0091 },
+    { from: "JPY", to: "EUR", rate: 0.0077 }
+];
 function convertCurrency() {
     // elementos de entrada
     const amount = parseFloat(document.getElementById("amount").value);
     const fromCurrency = document.getElementById("fromCurrency").value;
     const toCurrency = document.getElementById("toCurrency").value;
 
-    //array de objetos
-    const exchangeRates = [
-        { from: "USD", to: "EUR", rate: 0.85 },
-        { from: "USD", to: "JPY", rate: 110.25 },
-        { from: "EUR", to: "USD", rate: 1.18 },
-        { from: "EUR", to: "JPY", rate: 129.71 },
-        { from: "JPY", to: "USD", rate: 0.0091 },
-        { from: "JPY", to: "EUR", rate: 0.0077 }
-    ];
+    if (amount <= 0) {
+        Swal.fire("Por favor, ingresa una cantidad valida (no negativa).");
+        return;  
+    }
+
+   
 
     const rateObj = exchangeRates.find(rate => rate.from === fromCurrency && rate.to === toCurrency);
 
     if (!rateObj) {
-        document.getElementById("resultText").textContent = "Las tasas de cambio no están disponibles para estas divisas.";
+        Swal.fire("Las tasas de cambio no estan disponibles para estas divisas")
     } else {
         const convertedAmount = amount * rateObj.rate;
-        document.getElementById("resultText").textContent = `${amount.toFixed(2)} ${fromCurrency} equivale a ${convertedAmount.toFixed(2)} ${toCurrency}.`;
+        const resultMessage = `${amount.toFixed(2)} ${fromCurrency} equivale a ${convertedAmount.toFixed(2)} ${toCurrency}.`;    
+        Swal.fire(resultMessage);
     }
-
-    //resultado
-    document.getElementById("result").style.display = "block";
 }
+document.addEventListener("DOMContentLoaded", function () {
+    const convertirBoton = document.querySelector("#convertirBoton");
+    if (convertirBoton) {
+        convertirBoton.addEventListener("click", convertCurrency);
+    }
+});
 
 
 
