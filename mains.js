6a95f241d0faf8b71f92c9ab927952baba04d4e6
Agustin-garcia13 <agustingarciaaltamura@gmail.com>
2023-09-14 16:52:58 -0300

    le agrego fetch y nuevas divisas

--- mains.js
+++ mains.js
@@ -1,41 +1,63 @@
- //array de objetos
- const exchangeRates = [
-    { from: "USD", to: "EUR", rate: 0.85 },
-    { from: "USD", to: "JPY", rate: 110.25 },
-    { from: "EUR", to: "USD", rate: 1.18 },
-    { from: "EUR", to: "JPY", rate: 129.71 },
-    { from: "JPY", to: "USD", rate: 0.0091 },
-    { from: "JPY", to: "EUR", rate: 0.0077 }
-];
-function convertCurrency() {
-    // elementos de entrada
-    const amount = parseFloat(document.getElementById("amount").value);
-    const fromCurrency = document.getElementById("fromCurrency").value;
-    const toCurrency = document.getElementById("toCurrency").value;
+const currencies = ["USD", "EUR", "JPY", "GBP", "CAD"];
 
-    if (amount <= 0) {
-        Swal.fire("Por favor, ingresa una cantidad valida (no negativa).");
-        return;  
-    }
-
-   
-
-    const rateObj = exchangeRates.find(rate => rate.from === fromCurrency && rate.to === toCurrency);
-
-    if (!rateObj) {
-        Swal.fire("Las tasas de cambio no estan disponibles para estas divisas")
-    } else {
-        const convertedAmount = amount * rateObj.rate;
-        const resultMessage = `${amount.toFixed(2)} ${fromCurrency} equivale a ${convertedAmount.toFixed(2)} ${toCurrency}.`;    
-        Swal.fire(resultMessage);
-    }
-}
 document.addEventListener("DOMContentLoaded", function () {
+    const fromCurrencySelect = document.getElementById("fromCurrency");
+    const toCurrencySelect = document.getElementById("toCurrency");
+
+    for (let i = 0; i < currencies.length; i++) {
+        const option = document.createElement("option");
+        option.value = currencies[i];
+        option.textContent = currencies[i];
+        fromCurrencySelect.appendChild(option);
+    }
+
+    for (let i = 0; i < currencies.length; i++) {
+        const option = document.createElement("option");
+        option.value = currencies[i];
+        option.textContent = currencies[i];
+        toCurrencySelect.appendChild(option);
+    }
+
     const convertirBoton = document.querySelector("#convertirBoton");
     if (convertirBoton) {
         convertirBoton.addEventListener("click", convertCurrency);
     }
 });
 
+function convertCurrency() {
+    const amount = parseFloat(document.getElementById("amount").value);
+    const fromCurrency = document.getElementById("fromCurrency").value;
+    const toCurrency = document.getElementById("toCurrency").value;
 
+    if (amount <= 0) {
+        Swal.fire("Por favor, ingresa una cantidad valida (no negativa).");
+        return;
+    }
 
+    const customExchangeRates = {
+        USD: {
+            EUR: 0.85,
+            JPY: 110.0,
+        }
+    };
+
+    if (customExchangeRates[fromCurrency] && customExchangeRates[fromCurrency][toCurrency]) {
+        const rate = customExchangeRates[fromCurrency][toCurrency];
+        const convertedAmount = amount * rate;
+        const resultMessage = `${amount.toFixed(2)} ${fromCurrency} equivale a ${convertedAmount.toFixed(2)} ${toCurrency} (Tasa de cambio personalizada).`;
+        Swal.fire(resultMessage);
+    } else {
+        fetch(`https://open.er-api.com/v6/latest/${fromCurrency}`)
+            .then(response => response.json())
+            .then(data => {
+                const rate = data.rates[toCurrency];
+                const convertedAmount = amount * rate;
+                const resultMessage = `${amount.toFixed(2)} ${fromCurrency} equivale a ${convertedAmount.toFixed(2)} ${toCurrency} (Tasa de cambio de la API).`;
+                Swal.fire(resultMessage);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                Swal.fire('Ha ocurrido un error al obtener las tasas de cambio.');
+            });
+    }
+}
